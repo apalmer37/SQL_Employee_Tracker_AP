c295f03f977dee751dc4447e848474865c58e31a
Alex Palmer <alexpalmer37@gmail.com>
2021-10-26 20:11:06 -0400

    DB update. Added seeds

--- db/employeeDB.sql
+++ db/employeeDB.sql
@@ -4,22 +4,20 @@ USE employeeDB;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(37) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(37) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(8, 0) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    name_first VARCHAR(37) NOT NULL,
-    name_last VARCHAR(37) NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
-)
+);
